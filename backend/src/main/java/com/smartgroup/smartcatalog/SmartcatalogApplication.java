4f5c59d9e959e3e8e18c734c24ca6b6b648f15e4
Romulo <rhabbiati@hotmail.com>
2022-09-22 05:50:09 +0100

    Backend project created

---
+++ backend/src/main/java/com/smartgroup/smartcatalog/SmartcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.smartgroup.smartcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartgroup/smartcatalog/SmartcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgroup.smartcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
