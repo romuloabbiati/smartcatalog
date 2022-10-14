3d66dd301e537befd85e595d9715922942a6a874
Romulo <rhabbiati@hotmail.com>
2022-10-14 06:23:01 +0100

    Product repository tests update

--- backend/src/test/java/com/smartgroup/smartcatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/smartgroup/smartcatalog/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.smartgroup.smartcatalog.entities.Product;
 
@@ -18,7 +19,7 @@ public class ProductRepositoryTests {
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
 		// 1. Arrange
-		long existingId = 1L;
+		Long existingId = 1L;
 		
 		// 2. Act
 		productRepository.deleteById(existingId);
@@ -26,6 +27,21 @@ public class ProductRepositoryTests {
 		// 3. Assert
 		Optional<Product> product = productRepository.findById(existingId);
 		Assertions.assertFalse(product.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenNonExistingId() {
+		
+		
+		// 3. Assert
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			// 1. Arrange
+			Long nonExistingId = 30L;
+			
+			// 2. Act
+			productRepository.deleteById(nonExistingId);
+		});
+		
 	}
 	
 }
