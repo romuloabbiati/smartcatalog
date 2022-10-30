c749c1d0265e54e4f434665c28dc98fb15a5b9bb
Romulo <rhabbiati@hotmail.com>
2022-10-30 08:45:15 +0000

    Custom validation response

--- backend/src/main/java/com/smartgroup/smartcatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/smartgroup/smartcatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -50,18 +51,23 @@ public class ResourceExceptionHandler {
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<StandardError> methodArgumentNotValid(
+	public ResponseEntity<ValidationError> methodArgumentNotValid(
 			MethodArgumentNotValidException exception, 
 			HttpServletRequest request) {
 		
 		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
-		StandardError error = new StandardError();
+		ValidationError error = new ValidationError();
 		
 		error.setTimestamp(Instant.now());
 		error.setStatus(status.value());
 		error.setError("Validation exception!");
 		error.setMessage(exception.getMessage());
 		error.setPath(request.getRequestURI());
+		
+		for(FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+			error.getErrors()
+			.add(new FieldMessage(fieldError.getField(), fieldError.getDefaultMessage()));	
+		}
 		
 		return ResponseEntity.status(status).body(error);
 	}
