f1ce58a67999917c06029264d1f3991b19ecb404
Romulo <rhabbiati@hotmail.com>
2022-11-02 07:31:21 +0000

    Spring Security implementation

--- backend/src/main/java/com/smartgroup/smartcatalog/services/UserService.java
+++ backend/src/main/java/com/smartgroup/smartcatalog/services/UserService.java
@@ -2,11 +2,16 @@ package com.smartgroup.smartcatalog.services;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,8 +28,10 @@ import com.smartgroup.smartcatalog.services.exceptions.DatabaseException;
 import com.smartgroup.smartcatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
-
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -97,6 +104,18 @@ public class UserService {
 			Role role = roleRepository.getOne(roleDTO.getId());
 			user.getRoles().add(role);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		
+		if(user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("User not found: " + username);
+		}
+		logger.info("User found: " + username);
+		return user;
 	}
 	
 }
