fbb10c1b34b47c448a562786896257debec95ecd
Romulo <rhabbiati@hotmail.com>
2022-11-04 05:42:15 +0000

    Config to enable H2 config

--- backend/src/main/java/com/smartgroup/smartcatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/smartgroup/smartcatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.smartgroup.smartcatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,7 +16,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	private static final String[] PUBLIC = { "/oauth/token" };
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 	
@@ -22,6 +25,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
+	@Autowired
+	private Environment env;
+	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
@@ -29,15 +35,18 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		// config to enable H2 database
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 			.antMatchers(PUBLIC).permitAll()
 			.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
 			.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("ADMIN", "OPERATOR")
 			.antMatchers(ADMIN).hasRole("ADMIN")
 			.anyRequest().authenticated();
-			
 	}
-
-	
 	
 }
